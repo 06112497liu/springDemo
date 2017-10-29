ed9dc3e550bd824a68c9e7f0f74d13768cc8af5a
06112497liu <804912547@qq.com>
2017-10-29 22:09:01 +0800

    spring学习

--- springLearn/src/main/java/com/bbd/controller/SpittleController.java
+++ springLearn/src/main/java/com/bbd/controller/SpittleController.java
@@ -4,10 +4,8 @@ import com.bbd.data.Spittle;
 import com.bbd.data.SpittleRespository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
