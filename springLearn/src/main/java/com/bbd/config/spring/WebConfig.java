ede5ee48b48cc448cbd3709dfa1c5aefd301cf8c
06112497liu <804912547@qq.com>
2017-10-29 13:40:44 +0800

    spring学习

---
+++ springLearn/src/main/java/com/bbd/config/spring/WebConfig.java
@@ -0,0 +1,31 @@
+package com.bbd.config.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc // 启用SpringMvc
+@ComponentScan(basePackages = "com.bbd.controller") // 启用组件扫描
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver() { // 配置jsp视图解析器
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setExposeContextBeansAsAttributes(true);
+        return viewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) { // 配置静态资源处理
+        // 我们要求DispathcherServlet将对静态资源的请求转发到Servlet中默认的Servlet上，而不是使用DispatcherServlet本省来处理此类请求
+        configurer.enable();
+    }
+}
